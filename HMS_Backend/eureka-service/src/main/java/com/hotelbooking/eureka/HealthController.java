d55cf9b6dcac1d6c501aef239692fef3790feb06
Ishika <22052461@kiit.ac.in>
2025-12-28 00:53:47 +0530

    Added: Dummy route in API Gateway using Eureka service discovery Configured: Gateway routing with load-balanced service resolution

---
+++ HMS_Backend/eureka-service/src/main/java/com/hotelbooking/eureka/HealthController.java
@@ -0,0 +1,13 @@
+package com.hotelbooking.eureka;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "Discovery service is reachable";
+    }
+}
